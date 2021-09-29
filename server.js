9f1240f491ca2e39697a32977ffd82b0c8060d25
murshidazher <murshidazher@yahoo.com>
2021-09-30 04:33:45 +0530

    :sparkles: feat: version bump

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const fabObj = require("./math-logic/fibonacci-series");
 const app = express();
-
 
 app.use(cors({
   origin: '*'
@@ -17,6 +15,5 @@ app.use("/", require("./routes/poll.js"));
 
 app.listen(8080, function () {
   // simulate time to connect to other services
-  // let number = fabObj.calculateFibonacciValue(Number.parseInt(50));
   console.log("Listening on port 8080");
 });
