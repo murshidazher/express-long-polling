6affd0ba72be4db55f260574bcd650df60b9261c
murshidazher <murshidazher@yahoo.com>
2021-09-30 21:48:38 +0530

    :sparkles: feat: add pkill

--- src/monitor.js
+++ src/monitor.js
@@ -1,9 +1,9 @@
-const { Monitor } = require('forever-monitor');
+const { Monitor } = require('forever-monitor')
 
 class NodeMonitor {
     constructor() {
-        this.restartsCount = 0;
-        this.child = null;
+        this.restartsCount = 0
+        this.child = null
     }
 
     initiate() {
@@ -14,36 +14,36 @@ class NodeMonitor {
             path: '/home/ubuntu/forever/express-long-polling',
             killTree: true,
             append: true,
-            pidFile: '/home/ubuntu/forever/express-long-polling/pidfile',
+            pidFile: `/home/ubuntu/forever/express-long-polling/pidfile_${process.pid}`,
             logFile: 'forever.log',
             outFile: '/home/ubuntu/forever/express-long-polling/stdout.log',
             errFile: '/home/ubuntu/forever/express-long-polling/stderr.log',
             sourceDir: '/home/ubuntu/express-long-polling',
             cwd: '/home/ubuntu/express-long-polling',
             args: [this.restartsCount],
-        });
+        })
 
         this.child.on('restart', () => {
             // console.error('Forever restarting script for ' + child.times + ' time');
-        });
+        })
 
         this.child.on('exit:code', (code) => {
-            this.exit(code);
-        });
+            this.exit(code)
+        })
 
-        this.child.start();
+        this.child.start()
     }
 
     exit(code) {
-        this.restartsCount += 1;
+        this.restartsCount += 1
         // console.error('Forever detected script exited with code ' + code);
         // don't restart the script on SIGTERM
         if (code === 1) {
-            this.child.stop();
+            this.child.stop()
         } else {
-            this.child.args[1] = this.restartsCount;
+            this.child.args[1] = this.restartsCount
         }
     }
 }
 
-new NodeMonitor().initiate();
+new NodeMonitor().initiate()
